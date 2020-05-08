69606df01d95af822fea44e970a35a6d62b174e8
Janet-J <jkn6216@gmail.com>
2020-05-08 10:13:09 +0300

    add user interface logic

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ function Destination(place,location,landmark)
     this.location=location;
     this.landmark=landmark;
 }
+
+//user interface
+$("form.form-class").submit(function(event){
+event.preventDefault();
+var inputedPlace=$("input#destination-place").val();
+var inputtedLocation=$("input#destination-location").val();
+var inputtedLandmark=$("input#destinaion-Landmark").val();
+var newDestination=new Destination(inputedPlace,inputtedLocation,inputtedLandmark);
+$("ul.listOfPlaces").append("<li><span class='destinations'>" + newDestination.place + "</li></span>")
+$("input#destination-place").val("");
+$("input#destination-location").val("");
+$("input#destinaion-Landmark").val("");
+})
